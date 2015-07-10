dc5e167b84a2d98a9c39dc98a66b3ed6f180ac3f
Splice09 <jpf0005@gmail.com>
2015-07-09 19:04:14 -0500

    Created a program that calculates pi to the nth decimal place

---
+++ Project1/HelloWorld/HelloClass.cs
@@ -0,0 +1,27 @@
+﻿// arguments: A B C D
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.HelloWorld
+{
+    /* Test class for: console writing, command line argument reading, and basic loop implementation. */
+    class HelloClass
+    {
+        /*
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("You entered the following {0} command line arguments:", args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            //Sleep for console visibility
+            System.Threading.Thread.Sleep(5000);
+        }
+        */
+    }
+}
---
+++ Project1/NumbersPractice/PiToTheNth.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.NumbersPractice
+{
+    /* This class takes in an integer from the user and computes pi to the input number decimal place. */
+    class PiToTheNth
+    {
+        public static void Main(String[] args)
+        {
+            double pi;
+            int userNum; 
+            Console.WriteLine("How many decimals of Pi would you like to see?");
+            userNum = Convert.ToInt32(Console.ReadLine());
+            if (userNum > 0)
+            {
+                if (userNum < 162)
+                {
+                    pi = piCalc(userNum);
+                    Console.WriteLine("Pi to the " + userNum + " decimal point: " + pi);
+                    //Sleep for answer visability
+                    System.Threading.Thread.Sleep(10000);
+                }
+                else
+                {
+                    Console.WriteLine("Please input a value less than 162.");
+                    userNum = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please input a value greater than 0.");
+                userNum = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        /* The equation used to calculate pi is accurate up to 161 decimal places. */
+        public static double piCalc(int roundInt)
+        {
+            double a = .5*(23+(4*(Math.Sqrt(34))));
+            double b = .5*((19*Math.Sqrt(2)) + (7*Math.Sqrt(17)));
+            double c = 429 + (304*Math.Sqrt(2));
+            double d = .5*(627 + (442*Math.Sqrt(2)));
+            double u = (Math.Pow(a + (Math.Sqrt(Math.Pow(a,2) - 1)),2)) * (Math.Pow(b + (Math.Sqrt(Math.Pow(b, 2) - 1)), 2)) * (c + (Math.Sqrt(Math.Pow(c, 2) - 1))) * (d + (Math.Sqrt(Math.Pow(d,2) - 1)));
+            double pi = Math.Log((Math.Pow(2 * u, 6) + 24)) / Math.Sqrt(3502);
+            return Math.Round(pi, roundInt);
+             
+        }
+    }
+}
